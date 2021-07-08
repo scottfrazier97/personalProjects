0d19b22d5f54baf51478643d07ba8ad7b723cb8f
scottfrazier97 <68704290+scottfrazier97@users.noreply.github.com>
2021-07-07 21:52:55 -0500

    List practice

--- python_practice/pythonic.py
+++ python_practice/pythonic.py
@@ -25,8 +25,22 @@ numTest()
 light_on = True
 print(light_on)
 
+#LOOPING through a list
+candies = ["Sour Patch Kids", "Swedish Fish", "Reeses", "Sour Skittles", "Kazoozles"]
+candy_cart = []
+allowance = 3
 
+for candy in candies:
+    number = candies.index(candy)
+    print(f"|{number}| {candy}")
 
+currently_spent = 0
+while currently_spent < allowance:
+    currently_spent = currently_spent + 1
+    candy_prompt = int(input("Which candy would you like?"))
+    candy_cart.append(candies[candy_prompt])
+    
+print(f"You chose {candy_cart} as your candies. Enjoy!")
 
 
 
