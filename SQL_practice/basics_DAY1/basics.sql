7a48702f539577fcb6a08497e6841c2afc510d66
Scott Frazier <68704290+scottfrazier97@users.noreply.github.com>
2021-07-25 12:01:24 -0500

    Update basics.sql

--- SQL_practice/basics_DAY1/basics.sql
+++ SQL_practice/basics_DAY1/basics.sql
@@ -8,52 +8,52 @@ CREATE TABLE cities (
 	is_part_of_unitedStates BOOLEAN default false,
 	last_updated timestamp default localtimestamp
 );
--- INSERT INTO cities (city, state, population, is_part_of_unitedStates)
--- VALUES ('Alameda', 'CA', 79177, true),
--- 	('Mesa', 'AZ', 496401, true),
--- 	 ('Boerne', 'TX', 16056, true),
--- 	 ('Anaheim', 'CA', 352497, true),
--- 	 ('Tucson', 'AZ', 535677, true),
--- 	 ('Garland', 'TX', 238002, true);
+INSERT INTO cities (city, state, population, is_part_of_unitedStates)
+VALUES ('Alameda', 'CA', 79177, true),
+	('Mesa', 'AZ', 496401, true),
+	 ('Boerne', 'TX', 16056, true),
+	 ('Anaheim', 'CA', 352497, true),
+	 ('Tucson', 'AZ', 535677, true),
+	 ('Garland', 'TX', 238002, true);
 
 select * from cities
 
---DEC = decimal
+-- DEC = decimal
 
---select city from cities
+select city from cities
 
---select city from cities where state = 'AZ'
+select city from cities where state = 'AZ'
 
--- select city from cities where population < 100000
+select city from cities where population < 100000
 
--- -- select city from cities where state = 'CA'
--- -- AND population < 100000
+select city from cities where state = 'CA'
+AND population < 100000
 
---select * from cities limit 3
+select * from cities limit 3
 
---select * from cities where state = 'TX' or state = 'CA'
+select * from cities where state = 'TX' or state = 'CA'
 
---***UPDATING CERTAIN VALUES***
--- -- Update cities 
--- -- set is_part_of_unitedStates = false
--- -- where id = 6
+-- ***UPDATING CERTAIN VALUES***
+Update cities 
+set is_part_of_unitedStates = false
+where id = 6
 
---order by population DESC -- DESC = descending order, ASC = ascending. ASC is the default
---order by id
+order by population DESC -- DESC = descending order, ASC = ascending. ASC is the default
+order by id
 
--- alter table cities
--- add column have_visited BOOLEAN default false;
+alter table cities
+add column have_visited BOOLEAN default false;
 
--- -- Update cities
--- -- set have_visited = false
--- -- where id = 2;
+Update cities
+set have_visited = false
+where id = 2;
 
---***DROPPING COLUMN***
--- alter table cities
--- drop column useless_column;
+-- ***DROPPING COLUMN***
+alter table cities
+drop column useless_column;
 
---***DELETING ROWS***
--- DELETE FROM cities WHERE id = 6;
+-- ***DELETING ROWS***
+DELETE FROM cities WHERE id = 6;
 
 
 
