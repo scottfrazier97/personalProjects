d41c6bfa0f90b77034c3b8f575bae64534dc4880
Scott Frazier <scottfrazier97@yahoo.com>
2021-08-15 18:37:49 -0500

    Added Fizzbuzz and R files

--- python_practice/kph_to_mph.py
+++ python_practice/kph_to_mph.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 
-print("Welcome to the KPH to MPH converter")
+print("Welcome to the KPH to MPH converter!")
 print("-" * 5)
 
-user_prompt = float(input("To begin, enter a speed in MPH that you would like to see in KPH: "))
-conversion = round(user_prompt / 1.609, 2)
-
+user_speed_prompt = float(input("To begin, enter a speed in MPH that you would like to see in KPH: "))
+conversion = round(user_speed_prompt / 1.609, 2)
 print("-" * 5)
 
-print(f"MPH: {user_prompt} \nKPH: {conversion} ")
+print(f"MPH: {user_speed_prompt} \nKPH: {conversion} ")
+
