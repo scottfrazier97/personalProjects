1c2b8b3c7cadfe148c6ab608aed8824749e23624
scottfrazier97 <scottfrazier97@yahoo.com>
2022-04-29 15:47:02 -0500

    Editing FizzBuzz file

--- python_practice/fizzbuzz.py
+++ python_practice/fizzbuzz.py
@@ -1,12 +1,11 @@
-# #if value is divisible by 5, print Fizz
 for n in range(100):
-    if n % 5 == 0 and n % 3 == 0:
-        print("FizzBuzz")
+    if n % 5 == 0 and n % 3 == 0:   #If value is divisible by 5 and 3, print FizzBuzz
+        print(n, "FizzBuzz", sep="-")
         continue
-    elif n % 5 == 0:
-        print("Fizz")
+    elif n % 5 == 0:    #If value is divisible by just 5, print Fizz
+        print(n, "Fizz", sep="-")
         continue
     elif n % 3 == 0:
-        print("Buzz")
+        print(n, "Buzz", sep="-")   #If value is divisible by just 3, print Buzz
         continue
     print(n)
