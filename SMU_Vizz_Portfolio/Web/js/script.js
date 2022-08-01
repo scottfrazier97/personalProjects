99d168e949224191a106f996981b26e3361118f7
scottfrazier97 <scottfrazier97@yahoo.com>
2022-08-01 10:58:28 -0500

    making a lot of edits

--- SMU_Vizz_Portfolio/Web/js/script.js
+++ SMU_Vizz_Portfolio/Web/js/script.js
@@ -8,65 +8,73 @@ function mouseEvents(e) {
   mouse.x = e.pageX - bounds.left - scrollX;
   mouse.y = e.pageY - bounds.top - scrollY;
 }
+
 document.addEventListener("mousemove", mouseEvents);
+
+xcenter = canvas.width/2;
+ycenter = canvas.height/2;
 
 // draw design at x,y and rotated by angle
 function drawRotated(x, y, angle) {
+
   context.setTransform(1, 0, 0, 1, x, y);
   context.rotate(angle);
   
-//face
-context.fillStyle = '#ffc964 ';
-context.strokeStyle = 'black';
-context.lineWidth = 5;
-context.beginPath();
-context.arc(320, 240, 200, 0, 2 * Math.PI);
-context.fill();
-context.stroke();
-context.closePath();
-
-//eyes
-context.fillStyle = 'black';
-context.beginPath();
-context.arc(270, 175, 30, 0, 2 * Math.PI);
-context.fill();
-context.stroke();
-context.closePath();
-
-context.beginPath();
-context.arc(370, 175, 30, 0, 2 * Math.PI);
-context.fill();
-context.stroke();
-context.closePath();
-
-//mouth
-context.strokeStyle = 'black';
-context.lineWidth = 5;
-context.beginPath();
-context.arc(320, 240, 125, 0, -1 * Math.PI);
-context.stroke();
-context.closePath();
-
-//nose
-context.strokeStyle = 'black';
-context.lineWidth = 3;
-context.beginPath();
-context.arc(325, 255, 6.6, 0, -1 * Math.PI);
-context.stroke();
-context.closePath();
+  //face
+  context.fillStyle = '#ffc964 ';
+  context.strokeStyle = 'black';
+  context.lineWidth = 5;
+  context.beginPath();
+  context.arc(320, 240, 200, 0, 2 * Math.PI);
+  context.fill();
   context.stroke();
+  context.closePath();
+
+  //eyes
+  context.fillStyle = 'black';
+  context.beginPath();
+  context.arc(270, 175, 30, 0, 2 * Math.PI);
+  context.fill();
+  context.stroke();
+  context.closePath();
+
+  context.beginPath();
+  context.arc(370, 175, 30, 0, 2 * Math.PI);
+  context.fill();
+  context.stroke();
+  context.closePath();
+
+  //mouth
+  context.strokeStyle = 'black';
+  context.lineWidth = 5;
+  context.beginPath();
+  context.arc(320, 240, 125, 0, -1 * Math.PI);
+  context.stroke();
+  context.closePath();
+
+  //nose
+  context.strokeStyle = 'black';
+  context.lineWidth = 3;
+  context.beginPath();
+  context.arc(325, 255, 6.6, 0, -1 * Math.PI);
+  context.stroke();
+  context.closePath();
+  context.stroke();
+
+  
 }
 
 // render loop called 60 times a second
 function update(timer) {
   context.setTransform(1, 0, 0, 1, 0, 0); // reset transform
-  context.clearRect(0, 0, 300, 300);
+  //context.clearRect(0, 0, 300, 300);
 
   // get angle from center to mouse
-  var angle = Math.atan2(mouse.y - 150, mouse.x - 150);
+  var angle = Math.atan2(mouse.y - 375, mouse.x - 375);
 
   // draw rotated design
-  drawRotated(150, 150, angle);
+  
+  drawRotated(xcenter, ycenter, angle);
   requestAnimationFrame(update);
 }
 requestAnimationFrame(update);
--- SMU_Vizz_Portfolio/Web/js/sketch.js
+++ SMU_Vizz_Portfolio/Web/js/sketch.js
@@ -28,12 +28,9 @@ function setup() {
 
 function draw() {
     background(51);
-    r = random(255); // r is a random number between 0 - 255
-    g = random(100,200); // g is a random number betwen 100 - 200
-    b = random(100); // b is a random number between 0 - 100
-    ab = random(200,255); // a is a random number between 200 - 255
+    r = 255;
   
-    stroke(r,g,b,ab);
+    stroke(r);
     strokeWeight(10);
 
     line(0, a, width, a);
