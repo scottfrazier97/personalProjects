1b2d2c2c613a747f6dc9ce462ab24c2832119ac0
scottfrazier97 <scottfrazier97@yahoo.com>
2024-05-09 13:16:08 -0500

    Adding changes to rails.py file

--- python_practice/rails.py
+++ python_practice/rails.py
@@ -10,7 +10,7 @@
 
 total_blocks = 900
 
-for index, _ in enumerate(range(total_blocks)):
+for index, _ in enumerate(range(total_blocks)): #The underscore is a common placeholder convention here. It is still being assigned to the value of the enumerate function, but not actually being utilized in our code. Therefore, the "_" is just for the reader to see it and recognize as a placeholder.
     if index % 7 == 0:
         print(f"{index}: Powered Rail")
     else:
