973a28c0b83700605984a7d49dba785ed6dfb4bc
scottfrazier97 <scottfrazier97@yahoo.com>
2025-08-04 20:39:05 -0500

    Adding initial functions information to query file 5

--- SQL/2025/Lectures/5 - Functions by Data Type.sql
+++ SQL/2025/Lectures/5 - Functions by Data Type.sql
@@ -1,1 +1,17 @@
 --=== Functions by Data Type===--
+
+--1) Function Basics
+--a) Definition: Within SQL, a function takes in zero or more inputs, applies a calculation or a transformation
+--and ouputs a value
+--b) Can recognize functions by a keyword followed by ()
+--c) Best practice to capitalize functions so they stand out
+--d) Function categories:
+	--Aggregate functions:
+		--Applies a calculation to many rows of data and returns a single value
+		--Often used alongside GROUP BY
+	--Window Functions:
+		--Perform calculations across a window of rows
+	--General Functions:
+		--Performs calculations on all individual values within a column
+
+--2) Numeric Functions
