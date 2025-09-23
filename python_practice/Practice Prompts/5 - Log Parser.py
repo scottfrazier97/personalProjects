3b39f93760a0309a53e53870b44d9fc10b0fc969
scottfrazier97 <scottfrazier97@yahoo.com>
2025-09-22 23:39:49 -0500

    adding logs to log parser file

--- python_practice/Practice Prompts/5 - Log Parser.py
+++ python_practice/Practice Prompts/5 - Log Parser.py
@@ -1,2 +1,7 @@
 # You’re given a list of log lines, write a function that returns a dictionary of how 
 # many times each user logged in.
+
+logs = ["2025-09-20 UserA login", 
+ "2025-09-20 UserB logout", 
+ "2025-09-20 UserA logout", 
+ "2025-09-21 UserC login"]
