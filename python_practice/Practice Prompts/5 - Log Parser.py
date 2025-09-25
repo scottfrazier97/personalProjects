6d2bafaaef0819cbd889e35cebe6e96494e7b3ff
scottfrazier97 <scottfrazier97@yahoo.com>
2025-09-24 21:17:06 -0500

    Adding solution to log parser python problem

--- python_practice/Practice Prompts/5 - Log Parser.py
+++ python_practice/Practice Prompts/5 - Log Parser.py
@@ -2,6 +2,31 @@
 # many times each user logged in.
 
 logs = ["2025-09-20 UserA login", 
- "2025-09-20 UserB logout", 
- "2025-09-20 UserA logout", 
- "2025-09-21 UserC login"]
+        "2025-09-20 UserB logout", 
+        "2025-09-20 UserA logout", 
+        "2025-09-21 UserC login"]
+
+def login_counter(log_list):
+
+    tracker = {}
+
+    for item in log_list:
+        
+        log_status = item.split()[2]
+
+        if log_status == "login":
+
+            user = item.split()[1]
+
+            if user in tracker:
+                tracker[user] += 1
+            else:
+                tracker[user] = 1
+
+        else:
+            continue
+
+    print(tracker)
+
+login_counter(logs)     
+      
