4c16d506623a1f80359c81322445105db2fe0c86
scottfrazier97 <scottfrazier97@yahoo.com>
2025-10-04 22:18:18 -0500

    fnishing the word length dictionary python problem

--- python_practice/Practice Prompts/13 - Word Lengths.py
+++ python_practice/Practice Prompts/13 - Word Lengths.py
@@ -1,2 +1,15 @@
 #Given a list of words, return a dict mapping each word → its length
 
+words = ["cat", "dog", "cat"]
+
+tracker = {}
+
+for word in words:
+
+    tracker[word] = len(word)
+
+print(tracker)
+
+#Pythonic:
+# def word_lengths(words):
+#     return {word: len(word) for word in words}
