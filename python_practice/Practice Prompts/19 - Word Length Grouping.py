8c88f235278cf045a45c955f75c698f3db5f5930
scottfrazier97 <scottfrazier97@yahoo.com>
2025-10-08 10:42:03 -0500

    Adding word length grouping python problem

---
+++ python_practice/Practice Prompts/19 - Word Length Grouping.py
@@ -0,0 +1,24 @@
+#Given a list of words, group them by their length.
+
+words =  ["cat","dog","apple","pear"]
+
+def word_grouping(wordlist):
+
+    tracker = {}
+
+    for word in wordlist:
+
+        len_key = len(word)
+
+        if len_key not in tracker:
+            tracker[len_key] = []
+            tracker[len_key].append(word)
+        else:
+            tracker[len_key].append(word)
+
+    sorted_tracker = dict(sorted(tracker.items(), key=lambda item: item[0]))
+
+    return sorted_tracker
+
+result = word_grouping(words)
+print(result)
