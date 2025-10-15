d7d1a00e4009446ec275148cc7f70da29265a6c0
scottfrazier97 <scottfrazier97@yahoo.com>
2025-10-15 11:11:18 -0500

    finishing the longest word with duplicates handling python problem

--- python_practice/Practice Prompts/26 - Longest Word.py
+++ python_practice/Practice Prompts/26 - Longest Word.py
@@ -1,4 +1,4 @@
-# Return both the word and its length, ignoring punctuation.
+# Return both the word and its length, ignoring punctuation. Account for ties.
 
 def longest_word(sentence):
 
@@ -9,26 +9,27 @@ def longest_word(sentence):
 
         if char in invalid_chars:
             lower_string = lower_string.replace(char, "")
-        else:
-            continue
 
     #Removing spaces and duplicate words
     final_string = set(lower_string.split())
 
-    word_length = 0
-    longest_word = ""
-
+    max_word_length = 0
+    longest_words = set()
 
     for word in final_string:
 
-        length = len(word)
-        word_length = length
+        current_length = len(word)
 
+        if max_word_length < current_length:
+            max_word_length = current_length
+            longest_words.clear()
+            longest_words.add(word)
 
+        elif max_word_length == current_length:
+            longest_words.add(word)
 
-
-
-
-    return print(final_string)
+    return longest_words, max_word_length
     
-longest_word("What you've just said is one of the most insanely idiotic things I have ever heard. At no point in your rambling, incoherent response were you even close to anything that could be considered a rational thought. Everyone in this room is now dumber for having listened to it.")
+
+words, length = longest_word("What you've just said is one of the most insanely idiotic things I have ever heard. At no point in your rambling, incoherent response were you even close to anything that could be considered a rational thought. Everyone in this room is now dumber for having listened to it.")
+print(f"Longest word(s): {words}\nLength: {length}")
